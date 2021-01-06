737015317c462cf4c19a902cc9c915af13b112fd
Nikolai <35627380+destbg@users.noreply.github.com>
2021-01-06 17:57:06 +0200

    Update main.js

--- client/main.js
+++ client/main.js
@@ -2,6 +2,8 @@
   const video_image = document.getElementById('video-img');
   const socket = io();
 
+  socket.emit('not-server');
+
   socket.on('image', (image) => {
     console.log(image);
     video_image.src = image;
